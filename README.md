470ba270f21a7130790739049dc05b4fda9f9f82
Gustavo Ferreira <gustavogardel@gmai.com>
2016-08-10 12:23:28 -0700

    added GET/:id route

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
-#Express-App-APT_TODO
+#Express-App-API
 ####REST API
-
-//Write your own notes here
 
 
 //was created a middlewear where the app.use will be used in all app and to connect the module that we want to use when we want to use.
+
+
+//this was a custom middlewear called after the root route and before the function ...
+
 ```
 app.get('/about', middlewear.logger, function(req, res){
   res.send('<h1>Express About Page</h1>')
 })
 ```
-//this was a custom middlewear called after the root route and before the function ...
 
 
 //moving the middlewear to his own file .js after make sure it was working. now we have to module.exports the middlewear and also require it at the server.js (best practice)
+
+
+//'npm install body-parser' and is being required at the top of my server.js for the whole app
+
+//creted 1st GET /todos & tested it with POSTMAN by creating key/value with get REST. (collection->Enviroment->Route)
--- server.js
+++ server.js
@@ -2,16 +2,53 @@ var express = require('express');
 //creating express app
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
 var PORT = process.env.PORT || 3000;
 var middlewear = require('./middlewear');
 
+var todos = [
+  {
+    id: 1,
+    description: 'Teach REST API',
+    completed: false
+  },
+  {
+    id: 2,
+    description: 'Go eat health lunch',
+    completed: true
+  }
+]
 
 //this middlewear is now for the whole app!
 app.use(middlewear.requireAuthentication);
+//this is using the module body-parser in whole app
+app.use(bodyParser());
 
 app.get('/', function(req, res){
   res.send('<h1>Express Todo API</h1>')
 })
+
+app.get('/todos', function(req,res){
+  res.json(todos);
+})
+
+app.get('/todos/:id', function(req, res){
+  //creating a variable that will hold id from params object as integer
+  var todoId = parseInt(req.params.id);
+  var matchedTodo;
+  todos.forEach(function(todo){
+    if(todoId === todo.id){
+      matchedTodo = todo;
+    }
+  })
+  if(matchedTodo){
+    res.json(matchedTodo)
+  }else{
+    res.status(404).send();
+  }
+})
+
 
 app.get('/about', middlewear.logger, function(req, res){
   res.send('<h1>Express About Page</h1>')
